ffa28afdd64a4bde049c8dde50bf629175df907d
Andrew Goulstone <63809530+HackBear656@users.noreply.github.com>
2020-09-21 16:42:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
